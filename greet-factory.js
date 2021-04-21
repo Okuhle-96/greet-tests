ac72050cf705dfee6edab16985b6f3d14cad0739
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-04-21 11:02:25 +0200

    all

---
+++ greet-factory.js
@@ -0,0 +1,134 @@
+function greetFactoryFunction(){
+
+    var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
+    var theName = "";
+    var theNameObject = {};
+    var theCounter = 0;
+
+   var nameExists = "This name already exist!"
+
+    var theXhosa = "Mholo, ";
+    var theTsonga = "Ahee, ";
+    var theSotho = "Dumelang, ";
+
+    function setIsixhosa(xhosa){
+        theXhosa = xhosa;
+    }
+
+    function setSesotho(sotho){
+        theSotho = sotho;
+    }
+    function setXitsonga(tsonga){
+        theTsonga = tsonga;
+    }
+
+    function setName(name){
+        theName = name;
+    }
+
+    function setLanguage(language){
+        if(!language){
+            return "Please select your language!";
+        }if (language === 'isixhosa'){
+                greetIsiXhosa + getName();
+                theCounter++;
+        }if(language === "sesotho"){
+            greetSesotho + getName();
+            theCounter++;
+        }if (language === "xitsonga"){
+            greetXitsonga;
+            theCounter++;
+        }else if(userInput = ""){
+            return "this"
+        }
+        names[userInput.value] = 1;
+    }
+
+    function setCounter(userInput) {
+        
+            if (theNameObject[userInput] === undefined) {
+                theCounter++;
+                theNameObject[userInput] = 1;       
+            } else if (theNameObject.hasOwnProperty(userInput)){
+                return "This name already exists!";
+            }if (theNameObject[userInput] === ""){
+                return "Nothing to add!"
+            }
+            theNameObject[userInput] = 1;
+        }
+    
+    function greetIsiXhosa(){
+        return theXhosa + getName();
+    }
+
+    function greetSesotho(){
+        return theSotho + getName();
+    }
+
+    function greetXitsonga(){
+        return theTsonga + getName();
+    }
+
+    function getName(){
+        return theName;
+    }
+
+    function setErrorMessages(userInput){
+        if (userInput === ""){
+            return "Please enter your name!";
+        }if (userInput === !alphabets){
+            return "Please enter a valid name!";
+        }if (userInput === theNameObject.hasOwnProperty(userInput)){
+            return nameExists;
+        }
+    }
+
+    function getCounter(){
+        return theCounter;
+    }
+
+    function setLocalStorage(){
+        localStorage['usersGreeted'] = counter;
+        localStorage.setItem("userNames", JSON.stringify(names));
+    }
+
+    function getItem(){
+        if (localStorage['usersGreeted'] && localStorage['userNames']) {
+            theCounter = Number(localStorage['usersGreeted']);
+            theNameObject = JSON.parse((localStorage['userNames']));
+            }
+    }
+
+    function clearCounter(){
+        theCounter = 0;
+        theNameObject = {};
+    }
+
+
+    
+    return {
+        setIsixhosa,
+        setSesotho,
+        setXitsonga,
+
+        setCounter,
+        setName,
+        
+        setErrorMessages,
+
+        greetIsiXhosa,
+        greetSesotho,
+        greetXitsonga,
+
+        setLanguage,
+        getCounter,
+        getName,
+
+        setLocalStorage,
+        getItem,
+        clearCounter
+
+    }
+
+}
---
+++ greet.test.js
@@ -0,0 +1,145 @@
+describe ("Greeting Exercise Factory Function", function(){
+    describe ("Should be able to greet names entered", function(){
+        it('Should greet Okuhle', function(){
+
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("Okuhle");
+
+            assert.equal('Okuhle',testingGreet.getName());
+        })
+
+        it('Should greet Achuma', function(){
+
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("Achuma");
+
+            assert.equal('Achuma',testingGreet.getName());
+        })
+
+        it('Should greet Kamva', function(){
+
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("Kamva");
+
+            assert.equal('Kamva',testingGreet.getName());
+        })
+
+    
+    })
+
+    describe ("Should be able to greet in three languages", function(){
+        it('Should greet a user in IsiXhosa', function(){
+            let testingGreet = greetFactoryFunction();
+            assert.equal("Mholo, ",testingGreet.greetIsiXhosa());
+        })
+
+        it('Should greet a user in SeSotho', function(){
+            let testingGreet = greetFactoryFunction();
+            assert.equal('Dumelang, ',testingGreet.greetSesotho());
+        })
+
+        it('Should greet a user in Xitsonga', function(){
+            let testingGreet = greetFactoryFunction();
+            assert.equal('Ahee, ',testingGreet.greetXitsonga());
+        })
+
+        it('Should ask a user to select a language when its not selected', function(){
+            let testingGreet = greetFactoryFunction();
+            assert.equal('Please select your language!',testingGreet.setLanguage());
+        })
+    })
+
+    describe ("Should be able to count names greeted", function(){
+        it('Should increase the counter by one when one name is added', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setCounter(('Tsholo'));
+
+            assert.equal(1,testingGreet.getCounter());
+        })
+
+        it('Should increase the counter by three when three names sre added', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setCounter("Lulu");
+            testingGreet.setCounter("Soso");
+            testingGreet.setCounter("Mbali");
+
+            assert.equal(3,testingGreet.getCounter());
+        })
+
+        it('Should not increase the counter when a name is repeatedly inserted', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setCounter("Amanda");
+            testingGreet.setCounter("Mbali");
+            testingGreet.setCounter("Mbali");
+            testingGreet.setCounter("Lulu");
+            testingGreet.setCounter("Lulu");
+            testingGreet.setCounter("Soso");
+            testingGreet.setCounter("Soso");
+            testingGreet.setCounter("Soso");
+            
+            
+            
+
+            assert.equal(4,testingGreet.getCounter());
+        })
+
+
+    })
+
+    describe ("Clearing counter", function(){
+        it('Should be able reset counter to its preserved state', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setCounter("Okuhle");
+            testingGreet.setCounter("Achuma");
+            testingGreet.setCounter("Khanyisa");
+
+            testingGreet.clearCounter();
+
+            assert.equal(0,testingGreet.getCounter());
+        })
+
+    }) 
+
+    describe ("Should be able to return errors", function(){
+        it('Should be able to tell the user to set a its name when there is no input', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("");
+
+            testingGreet.getName();
+
+            assert.equal("Please enter your name!", testingGreet.setErrorMessages());
+        })
+
+        it('Should be able to tell a user if the name has been greeted', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("Okuhle");
+            testingGreet.setName("Okuhle");
+
+            testingGreet.getName();
+
+            assert.equal("This name already exists!", testingGreet.setErrorMessages());
+        })
+
+        it('Should ask a user to enter a valid name when the user inputs characters or numbers', function(){
+            let testingGreet = greetFactoryFunction();
+
+            testingGreet.setName("!@#$%^&*");
+            testingGreet.setName("845121");
+
+            testingGreet.getName();
+
+            assert.equal("Please enter a valid name!", testingGreet.setErrorMessages());
+        })
+
+
+    }) 
+})
